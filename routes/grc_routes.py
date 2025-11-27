bf6f40f1e59d68b07f7016207217b57fbfbfaf4e
katepaul685 <katepaul685@gmail.com>
2025-11-27 21:31:36 +0300

    Added frontend files to the project

--- routes/grc_routes.py
+++ routes/grc_routes.py
@@ -15,22 +15,21 @@ except Exception as e:
     print(f"Error configuring AI: {e}")
     model = None
 
-# ROUTE 1: Generate a Policy (Protected for Admins)
+# ROUTE 1: Generate a Policy
 @grc_bp.route('/generate-policy', methods=['POST'])
 @jwt_required()  # User must be logged in
 def generate_policy():
-
-    # RBAC Check
-   # claims = get_jwt()
-   # user_role = claims.get("role")
-
-   # if user_role != "admin":
-         return jsonify({"error": "Access denied. Admin role required."}), 403
+    
+    # RBAC Check (Disabled for Hackathon Demo)
+    # claims = get_jwt()
+    # user_role = claims.get("role")
+    # if user_role != "admin":
+    #      return jsonify({"error": "Access denied. Admin role required."}), 403
 
     # If they are an admin, proceed...
     data = request.json
     description = data.get('company_description')
-
+    
     if not description:
         return jsonify({"error": "Description is required"}), 400
     if not model:
@@ -45,18 +44,18 @@ def generate_policy():
     try:
         response = model.generate_content(prompt)
         policy_text = response.text
-
+        
         # Save to Database
         new_policy = Policy(title="AI Generated Policy", content=policy_text)
         db.session.add(new_policy)
         db.session.commit()
 
         return jsonify(new_policy.to_dict()), 201
-
+        
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# ROUTE 2: Get All Policies (Anyone logged in can see)
+# ROUTE 2: Get All Policies
 @grc_bp.route('/policies', methods=['GET'])
 @jwt_required() # User must be logged in
 def get_policies():
